3f209d7723d78fdf5ade3f038be9fc9049103ce1
Keith Pierce <krp0724@gmail.com>
2011-01-04 12:35:49 -0600

    Corrected links in footer

--- sites/all/themes/mifos/page-front.tpl.php
+++ sites/all/themes/mifos/page-front.tpl.php
@@ -208,31 +208,7 @@
 
     </div></div> <!-- /#main-inner, /#main -->
 
-    <div id="footer"><div id="footer-inner" class="region region-footer">
-
-      <?php if ($footer_message): ?>
-        <div id="footer-message"><?php print $footer_message; ?></div>
-      <?php endif; ?>
-      
-      <div id="gray-footer">
-        <?php print $footer; ?>
-        
-        <p id="email-signup">Sign up for our <a href="#">e-mail list</a> For updates. </p>
-      </div> <!-- gray-footer -->
-      
-      <div id="footer-extras">
-        <p id="footer-logo"><a href="#"><img src="<?php echo base_path() . path_to_theme() ?>/images/logo_grameen.png" /></a></p>
-
-        <div id="copyright">
-          <?php print $footer_links ?>
-          <p>Copyright &copy; <?php echo date('Y') ?> Grameen Foundation. <a href="#">PRIVACY POLICY</a></p>
-        </div>
-
-        <p id="donate-text">Help support innovations that help end the cycle of poverty.</p>
-        <p id="donate-button"><a href=""><img src="<?php echo base_path() . path_to_theme() ?>/images/btn_donate.png" alt="Donate Now"></a></p>
-      </div> <!-- footer-extras -->
-
-    </div></div> <!-- /#footer-inner, /#footer -->
+<?php include "footer.php" ?>
 
   </div></div> <!-- /#page-inner, /#page -->
 
