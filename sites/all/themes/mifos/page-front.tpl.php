8c66d045e809117953bc1016e73863283036f390
Keith Pierce <krp0724@gmail.com>
2011-02-11 10:02:17 -0600

    Revert "Modify template files to show view "banner" as front page's content"
    This reverts commit cde8621e811e0d3de7d442a11da22d98525b2d9d.

--- sites/all/themes/mifos/page-front.tpl.php
+++ sites/all/themes/mifos/page-front.tpl.php
@@ -161,7 +161,7 @@
               <div id="content_right_sidebar"><?php echo $content_right ?></div>
             <?php endif ?>
 
-            <?php print $banner_view; ?>
+            <?php print $content; ?>
           </div>
           <img src="<?php echo base_path() . path_to_theme() ?>/images/banner_left.png" alt="" class="content-bottom-banner" />
           <div class="clear"></div>
