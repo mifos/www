407964b423efea72cbcfd060f6d7f2c28b943c70
Keith Pierce <krp0724@gmail.com>
2011-04-23 07:12:37 -0500

    MIFOSWEB-26: Improved layout of news log:
    * Remove horizontal lines between stories
    * Narrowed story block by 20% for improved readability
    * Clear previous story's floated picture so it doesn't spill into next story
    * Put grey block at bottom of each story to more cleanly separate stories
    * Put comment count and "Posted by" entry in grey block at bottom
    
    See http://mifosforge.jira.com/browse/MIFOSWEB-26

---
+++ sites/all/themes/mifos/views/views-view-fields--news-log.tpl.php
@@ -0,0 +1,36 @@
+<?php
+// $Id: views-view-fields.tpl.php,v 1.6 2008/09/24 22:48:21 merlinofchaos Exp $
+/**
+ * @file views-view-fields.tpl.php
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->separator: an optional separator that may appear before a field.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php
+  //dsm($fields);
+?>
+<div class="news-entry">
+    <h3 class="news-entry-title"><?php print $fields['title']->content;?></h3>
+  <div class="news-entry-teaser"><?php print $fields['teaser']->content;?></div>
+    <p class="news-entry-more-link"><?php print $fields['view_node']->content;?></p>
+    <div class="news-entry-footer">
+        <p class="news-entry-comment-count"><?php print $fields['comment_count']->content;?></p>
+        <p class="news-entry-posted-by">
+          Posted by <?php print $fields['name']->content;?> 
+          on <?php print $fields['created']->raw;?></p>
+    </div>
+</div>
+
